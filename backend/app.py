b698c48ae5e54a862849ba3f04fde59a7ffc3d06
Yoori <noofy.fyda@icloud.com>
2026-06-19 17:35:47 +0800

    add valeu

--- backend/app.py
+++ backend/app.py
@@ -267,7 +267,10 @@ def search():
 
 @app.route("/calculator")
 def calculator():
-    return send_from_directory(os.path.join(BASE_DIR, "static"), "della_calculator.html")
+    return send_from_directory(
+        os.path.join(BASE_DIR, "static"),
+        "della_calculator.html"
+    )
 
 # =========================================================
 # RUN
